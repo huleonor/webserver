52e5cd4abceca16f004f4e846766543189338399
carlaugu <carlosleal2000@gmail.com>
2026-06-15 21:30:20 +0000

    refactor: change normalizeSlash

--- src/core/utils.cpp
+++ src/core/utils.cpp
@@ -2,8 +2,6 @@
 
 void	normalizeSlash(std::string& str)
 {
-	if (!str.empty() && str[0] == '/')
-		str.erase(0, 1);
 	if (!str.empty() && str[str.size() - 1] == '/')
 		str.erase(str.size() - 1);
 }
